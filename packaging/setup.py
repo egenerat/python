c302961e1e3ac8f512f9e4d1d95075c236110202
egenerat <emile.generat@outlook.com>
2019-12-08 13:44:02 +0000

    Pip package

---
+++ packaging/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='addition',
+      version='0.1',
+      description='Very useful module',
+      url='http://github.com/egenerat/python',
+      author='Emile Generat',
+      author_email='emile.g@outlook.com',
+      license='MIT',
+      packages=['addition'],
+      zip_safe=False)
