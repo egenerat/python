f8a5d0dd65ed47f3bde765dd93bb38a40ab5cdd4
Emile Generat <egenerat@users.noreply.github.com>
2020-12-22 20:38:53 +0000

    zip function

---
+++ zip.py
@@ -0,0 +1,5 @@
+a = ["Paris", "London", "New York"]
+b = ["France", "United Kingdom", "United States of America"]
+
+print(list(zip(a, b)))
+# [('Paris', 'France'), ('London', 'United Kingdom'), ('New York', 'United States of America')]
